dd46a0f0b774bad56f06446221d600088edca702
NYZ29 <u.nail2006@mail.ru>
2026-09-15 14:24:21 +0300

    https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,16 @@
-public class Solution {
-    public int[] wave(int[] A) {
-        java.util.Arrays.sort(A);
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] smaller = new int[n];
 
-        for (int i = 0; i < A.length - 1; i += 2) {
-            int temp = A[i];
-            A[i] = A[i + 1];
-            A[i + 1] = temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                if (nums[i] > nums[j]) smaller[i]++;
+            }
         }
 
-        return A;
+        return smaller;
     }
 }
