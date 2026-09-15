edc8a2181b95da1b2721f3a9e7b4668b0357dd4c
NYZ29 <u.nail2006@mail.ru>
2026-09-15 22:11:36 +0300

    https://acmp.ru/index.asp?main=task&id_task=43

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -9,29 +8,26 @@ public class Main {
         try {
             List<String> lines = Files.readAllLines(Paths.get("INPUT.TXT"));
 
-            int k = Integer.parseInt(lines.get(0).trim());
-            StringBuilder sb = new StringBuilder();
+            String str = lines.get(0).trim();
+            int maxLen = 0;
+            int curLen = 0;
 
-            for (int i = 0; i < k; i++) {
-                String[] part = lines.get(i + 1).trim().split("\\s+");
-                int n = Integer.parseInt(part[0]);
-                int m = Integer.parseInt(part[1]);
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
 
-                int d = formula(n, m);
-
-                sb.append(d).append('\n');
+                if (c == '0') {
+                    curLen++;
+                    if (maxLen < curLen) maxLen = curLen;
+                }
+                else curLen = 0;
             }
 
-            System.out.println(sb);
-            Files.writeString(Paths.get("OUTPUT.TXT"), sb);
+            System.out.println(maxLen);
+            Files.writeString(Paths.get("OUTPUT.TXT"), Integer.toString(maxLen));
 
         } catch (IOException e) {
             System.err.println("Ошибка работы с файлами: " + e.getMessage());
             e.printStackTrace();
         }
-    }
-
-    private static int formula(int n, int m) {
-        return (19 * m + (n + 239) * (n + 366) / 2);
     }
 }
