cc76652ae59307637a3af73fe5bc3cd6deb05407
NYZ29 <u.nail2006@mail.ru>
2026-09-15 21:13:28 +0300

    https://acmp.ru/index.asp?main=task&id_task=4

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,22 @@
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("INPUT.TXT"));
 
-            String[] words = lines.getFirst().split(" ");
-            int[] numbers = new int[words.length];
+            String line = lines.get(0);
+            int digit = Integer.parseInt(line.trim());
+            int number = digit * 100 + 90 + (9 - digit);
 
-            for (int i = 0; i < words.length; i++) {
-                numbers[i] = Integer.parseInt(words[i]);
-            }
+            System.out.println(number);
+            Files.writeString(Paths.get("OUTPUT.TXT"), Integer.toString(number) + "\n");
 
-            int sum = 0;
-            for (int num : numbers) sum += num;
-
-            Files.writeString(Paths.get("output.txt"), Integer.toString(sum));
         } catch (IOException e) {
-            System.err.println("Файл не открылся");
+            System.err.println("Ошибка работы с файлами: " + e.getMessage());
         }
     }
 }
