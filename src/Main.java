5fab5ed19b450573f6ca268abfc085225d4949c4
NYZ29 <u.nail2006@mail.ru>
2026-09-15 21:55:23 +0300

    https://acmp.ru/index.asp?main=task&id_task=35

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -8,22 +9,29 @@ public class Main {
         try {
             List<String> lines = Files.readAllLines(Paths.get("INPUT.TXT"));
 
-            String[] words = lines.get(0).trim().split(" ");
-            int[] numbers = new int[words.length];
+            int k = Integer.parseInt(lines.get(0).trim());
+            StringBuilder sb = new StringBuilder();
 
-            for (int i = 0; i < words.length; i++) {
-                numbers[i] = Integer.parseInt(words[i]);
+            for (int i = 0; i < k; i++) {
+                String[] part = lines.get(i + 1).trim().split("\\s+");
+                int n = Integer.parseInt(part[0]);
+                int m = Integer.parseInt(part[1]);
+
+                int d = formula(n, m);
+
+                sb.append(d).append('\n');
             }
 
-            String answer = (numbers[0] * numbers[1] == numbers[2])
-                    ? "YES" : "NO";
-
-            System.out.println(answer);
-            Files.writeString(Paths.get("OUTPUT.TXT"), answer);
+            System.out.println(sb);
+            Files.writeString(Paths.get("OUTPUT.TXT"), sb);
 
         } catch (IOException e) {
             System.err.println("Ошибка работы с файлами: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private static int formula(int n, int m) {
+        return (19 * m + (n + 239) * (n + 366) / 2);
     }
 }
