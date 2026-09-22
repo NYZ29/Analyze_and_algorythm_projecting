d773ec6c0b645ce92c642b92fb0dc0fa2fb7900e
NYZ29 <u.nail2006@mail.ru>
2026-09-23 01:07:26 +0300

    https://acmp.ru/index.asp?main=task&id_task=148

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,32 @@ public class Main {
         try {
             List<String> file = Files.readAllLines(Paths.get("INPUT.TXT"));
 
-            long n = Long.parseLong(file.get(0).trim());
-            long min = (n / 6) + (7 - (n % 6)) % 7;
-            long max = n * 6;
-            String answer = min + " " + max;
+            String[] numbers = file.get(0).trim().split("\\s+");
+
+            int a = Integer.parseInt(numbers[0]);
+            int b = Integer.parseInt(numbers[1]);
+
+            String answer = Integer.toString(NOD(a, b));
 
             Files.writeString(Paths.get("OUTPUT.TXT"), answer);
 
         } catch (IOException e) {
 
         }
+    }
+
+    private static int NOD(int a, int b) {
+        int min = Math.min(a, b);
+        int max = a + b - min;
+
+        while (min > 0 && max > 0) {
+            if (max % min == 0) return min;
+
+            int temp = max % min;
+            max = min;
+            min = temp;
+        }
+
+        return 1;
     }
 }
