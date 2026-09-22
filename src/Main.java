77da6aad96267a26974a806eea3264127359dd9f
NYZ29 <u.nail2006@mail.ru>
2026-09-22 22:36:55 +0300

    https://acmp.ru/index.asp?main=task&id_task=331

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> A1 = new ArrayList<>(Arrays.asList(0, 1, 0, 3, 12));
+        try {
+            List<String> file = Files.readAllLines(Paths.get("INPUT.TXT"));
 
-        ArrayList<Integer> A2 = new ArrayList<>(Arrays.asList(0));
+            String[] start = file.get(0).trim().split("\\s*:");
+            String[] period = file.get(1).trim().split("\\s+");
 
-        System.out.println(Solution.solve(A1));
-        System.out.println(Solution.solve(A2));
+            int endMinute = Integer.parseInt(start[1]) + Integer.parseInt(period[1]);
+            int endHour = (Integer.parseInt(start[0]) + Integer.parseInt(period[0]) + (endMinute / 60)) % 24;
+            endMinute %= 60;
+
+            StringBuilder end = new StringBuilder();
+
+            if (endHour < 10) end.append(0).append(endHour);
+            else end.append(endHour);
+            end.append(":");
+
+            if (endMinute < 10) end.append(0).append(endMinute);
+            else end.append(endMinute);
+
+            System.out.println(end);
+            Files.writeString(Paths.get("OUTPUT.TXT"), end);
+
+        } catch (IOException e) {
+            System.err.println("Ошибка работы с файлами: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
